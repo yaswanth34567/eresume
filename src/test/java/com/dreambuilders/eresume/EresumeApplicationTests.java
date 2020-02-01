238e07785531e3c67661c1a8296af2e18a8fc920
krishna <krishna192168@gmail.com>
2020-02-01 19:14:42 +0530

    skull for eresume

---
+++ src/test/java/com/dreambuilders/eresume/EresumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreambuilders.eresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EresumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
